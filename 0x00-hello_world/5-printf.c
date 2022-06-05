194c1c4e8a1544a77ee61fc826e2ca4392783dbb
dannysoft-code <ogbugofejiro@gmail.com>
2022-06-05 05:57:52 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-print("with proper grammer, but the outcome is a piece of art,\n");
-retun (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
