bfbf115f3be1f4f09a6b30b72a4d504b7f3330ca
dannysoft-code <ogbugofejiro@gmail.com>
2022-06-13 16:10:14 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -upadte the value of the pointer 98
+ * the poniter is pointing to to 98
+ * @n: this is a poniter
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
