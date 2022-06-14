135fddb3ba5aa026fd24909e435517e72a8360f3
dannysoft-code <ogbugofejiro@gmail.com>
2022-06-14 06:09:23 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,9 @@
-include "main.h"
+#include "main.h"
 
 /**
  * swap_int - This changes the positions of the values
  * @a: This is a pointer to the first value
  * @b: This is a pointer to the second value
- *
- * Return: Null void
  */
 void swap_int(int *a, int *b)
 {
