dd162228d8a909d0180e7150e0e0b96d42286f28
dannysoft-code <ogbugofejiro@gmail.com>
2022-06-15 07:33:16 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - Flips an array
+ * @a: This array is to be flipped
+ * @n: The total number of elements in the array
+ *
+ * Return: Null Void
+ */
+
+void reverse_array(int *a, int n)
+{
+int i, first, last, keep, counts;
+
+counts = n / 2;
+first = 0;
+last = n - 1;
+
+for (i = 0; i < counts; i++)
+{
+keep = a[first];
+a[first] = a[last];
+a[last] = keep;
+first++;
+last--;
+}
+}
