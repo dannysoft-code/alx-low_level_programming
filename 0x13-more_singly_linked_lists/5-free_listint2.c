dcc864262a8d8340aa8885a77f20e8457372ede2
dannysoft-code <ogbugofejiro@gmail.com>
2022-07-20 15:50:58 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,31 +6,29 @@
  *
  * Return: Null void
  */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *node;
-	listint_t *forward;
-
-	if (*head == NULL)
-		return;
-
-	node = *head;
-	forward = node->next;
-
-	while (node != NULL)
-	{
-		free(node);
-		node = forward;
-		if (forward->next != NULL)
-		{
-			forward = forward->next;
-		}
-		else
-		{
-			free(forward);
-			node = NULL;
-		}
-	}
-	*head = NULL;
-	head = NULL;
+listint_t *node;
+listint_t *forward;
+if (*head == NULL)
+return;
+node = *head;
+forward = node->next;
+while (node != NULL)
+{
+free(node);
+node = forward;
+if (forward->next != NULL)
+{
+forward = forward->next;
+}
+else
+{
+free(forward);
+node = NULL;
+}
+}
+*head = NULL;
+head = NULL;
 }
