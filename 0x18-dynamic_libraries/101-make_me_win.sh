dbf862e78ec395515d310a3746c562f218cb00bb
dannysoft-code <ogbugofejiro@gmail.com>
2022-09-09 15:35:44 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/charlykso/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+wget -P https://github.com/dannysoft-code/alx-low_level_programming/0x18-dynamic_libraries
 export LD_PRELOAD=/tmp/preload.so
